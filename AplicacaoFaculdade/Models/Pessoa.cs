c39e6c77066fb1f59c6752b538ebe430aeb6a7b3
Mauricio Barbosa <mauricio.coelho@etec.sp.gov.br>
2019-06-28 11:27:58 -0300

    Starting changing Database Structure

--- AplicacaoFaculdade/Models/Pessoa.cs
+++ AplicacaoFaculdade/Models/Pessoa.cs
@@ -9,9 +9,20 @@ namespace AplicacaoFaculdade.Models
         public int? Id { get; set; }
         public string Nome { get; set; }
         public string SobreNome { get; set; }
-        public bool Juridica { get; set; }
-        public bool Sexo { get; set; }
+        public int Sexo { get; set; }
         public DateTime Nascimento { get; set; }
+        public long? Telefone { get; set; }
+        public long? Celular { get; set; }
+        public long? Cnpj { get; set; }
+        public int? Cpf { get; set; }
+        public int? Rg { get; set; }
+        public string Endereco { get; set; }
+        public int? Cep { get; set; }
+        public int? Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
         public bool? Status { get; set; }
+
     }
 }
--- AplicacaoFaculdade/Models/TipoTelefone.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace AplicacaoFaculdade.Models {
-    public class TipoTelefone {
-        public int? Id { get; set; }
-        public string Nome { get; set; }
-        public bool? Status { get; set; }
-    }
-}
